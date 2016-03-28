37267e7f0d1493889778bdc4a8b993712d2dfa8b
dms163 <dms163@pitt.edu>
2016-03-28 19:43:55 -0400

    Final commit

--- template/quiz2.js
+++ template/quiz2.js
@@ -4,35 +4,62 @@
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	$mouseover = $('.mouse-over');
+	$click     = $('.click');
+	$submit       = $('.submit');
+	$cookie = document.cookie.substring(document.cookie.indexOf('=')+1, document.cookie.length); // get only value
 
-	$mouseover.on('mouseover', function() {
-		$this = $(this);
-		$(this).html('Scrooge McDuck!');
-		$(this).height($(this).height() + 50);
+	$("#titles").text($cookie);
+
+	$mouseover.mouseover(function () {
+		//$this = $(this);
+		$('.mouse-over').html('Scrooge McDuck!');
+		$('.mouse-over').height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+
+
+	$click.click( function() {
+		$('.click').html('Peace Out!')
+		$('.click').fadeout(1500);
 		return false;
 	});
 
 	$submit.on('submit', function(e) {
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+			$(this).find('input').each(function() {
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append("<h2>Congratulations! You've entered some text!</h2>");
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
+	$('#btnTitle').click(function(){
+		$.getJSON( "http://www.mattbowytz.com/simple_api.json", {data: 'quizData'}, function( response ) {
+			console.log(response);
+			console.log(response.data);
+
+			// get a random value from the returned data
+			var rndNum = Math.floor(Math.random() * response.data.length) + 1;
+
+			// set the paragraph to show the random item
+			$("#titles").text(response.data[rndNum]);
+
+			// rename the button
+			$("#btnTitle").text("Change It");
+
+			// show the "Keep It" button
+			$("#btnKeep").show().click(function (){
+				// set cookie
+				document.cookie = 'value=' + response.data[rndNum] + ';path=\\';
+			});
+		});
+	});
+
+	$(document).on("ready", function() {
+		setTimeout(function(){
+			$("timeout").fadeIn("slow");
 		}, 1000);
 	});
 
